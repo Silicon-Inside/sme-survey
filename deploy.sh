644b48ad9e4d692917e970bee9cab28fd1cf553e
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-06-20 14:49:05 +0530

    remove unnecessary comments from gitignore

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,10 @@
 # git commit -am "Commit Message"
 # git push
 # firebase deploy
+
+
+
+
+# notes
+# https://github.com/digvijayu/spike_node_read_data_from_google_sheets
+# firebase serve --only functions
